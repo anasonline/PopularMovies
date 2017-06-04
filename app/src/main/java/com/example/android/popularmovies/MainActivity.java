039624b59ee9850947c2599e4756b9ecf08ca44c
Anas <anas.marrawi@gmail.com>
2017-06-04 14:36:41 +0200

    Add the MovieAdapter Add the movie_item.xml layout Add the GridLayout Add test images to test the GridLayout

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.android.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MovieAdapter mAdapter;
+    private GridView mGridView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find a reference to the GridView in the layout
+        mGridView = (GridView) findViewById(R.id.movies_grid);
+
+        // Create a new adapter that takes an empty list of movies as input
+        mAdapter = new MovieAdapter(MainActivity.this, new ArrayList<Movie>());
+
+        // Set the adapter on the GridView
+        mGridView.setAdapter(mAdapter);
     }
+
+
 }
+
---
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.popularmovies;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by anas on 04.06.17.
+ */
+
+public class MovieAdapter extends ArrayAdapter<Movie> {
+
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+    public MovieAdapter(Activity context, List<Movie> movies) {
+
+        super(context, 0, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if there is an existing list item view (called convertView) that we can reuse,
+        // otherwise, if convertView is null, then inflate a new list item layout.
+        View gridItemView = convertView;
+
+        if (gridItemView == null) {
+            gridItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.movie_item, parent, false);
+        }
+
+        // Find the movie at the given position in the movie grid
+        Movie currentMovie = getItem(position);
+
+        // Find the ImageView with the ID: movie_image
+
+        ImageView movieImage = (ImageView) gridItemView.findViewById(R.id.movie_image);
+        //movieImage.setImageResource(currentMovie.getImageUrl());
+
+        return gridItemView;
+    }
+
+}
